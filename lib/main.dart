3dead2d5239bd34e8a90fe7231eaedfb41b5bfb7
Saeedi <masoud.saeedi.dev@gmai.com>
2023-12-05 16:23:24 +0330

    Deeplink in Flutter 3.16.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MaterialApp.router(routerConfig: router));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter',
-      theme: ThemeData(
-        fontFamily: 'YekanBakhFaNumRegular',
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Enter Your Title Here ...'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Sample text',
-            ),
-          ],
+/// This handles '/' and '/details'.
+final router = GoRouter(
+  debugLogDiagnostics: true,
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (_, __) => Scaffold(
+        appBar: AppBar(title: const Text('Home Screen')),
+        body: Builder(
+          builder: (context) {
+            return Center(
+              child: ElevatedButton(
+                  onPressed: () => context.push('/details'),
+                  child: const Text('Go to details page!')),
+            );
+          },
         ),
       ),
-    );
-  }
-}
+    ),
+    GoRoute(
+      path: '/details',
+      builder: (_, __) => Scaffold(
+        appBar: AppBar(title: const Text('Details Screen')),
+        body: const Center(
+          child: Text('You are in details page'),
+        ),
+      ),
+    )
+  ],
+);
